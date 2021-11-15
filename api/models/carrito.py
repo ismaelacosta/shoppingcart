ec83146254e39cd81d1d53ad70ffa6ebfd0d927e
Ismael Acosta <acostalopezrobertoismael@gmai.com>
2021-11-14 19:34:53 -0700

    Se corrigio las validaciones en carrito

--- api/models/carrito.py
+++ api/models/carrito.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Carrito(BaseModel):
-    id_carrito : Optional[int] = None
     id_cliente : int 
     id_producto : int
     nombre_producto : str
     proveedor : str
     cantidad_de_unidades : int
+    precio_por_unidad: int
     tipo_producto : str
-     
+
